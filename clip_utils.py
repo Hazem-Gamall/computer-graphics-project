6b8e518eba4affad123435d0a140310c9276dbef
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-12-28 17:44:45 +0200

    liang-barsky implemented

--- clip_utils.py
+++ clip_utils.py
@@ -68,12 +68,57 @@ def cohen_sutherland(p1: Tuple, p2: Tuple, window_max: Tuple, window_min: Tuple)
             p1 = tuple(map(round, (x,y)))
         else:
             p2 = tuple(map(round, (x,y)))
+
+
+def liang_barsky(p1: Tuple, p2: Tuple, window_max: Tuple, window_min: Tuple) -> Tuple:      
+    xmax, ymax = window_max
+    xmin, ymin = window_min
+    u1, u2 = 0, 1
+    (x1,y1),(x2,y2) = p1, p2
+    dx = x2-x1
+    dy = y2-y1
+    
+    for i in range(4):
+        if i == 0:
+            p = -dx
+            q = x1-xmin
+        elif i == 1:
+            p = dx
+            q = xmax - x1
+        elif i == 2:
+            p = -dy
+            q = y1 - ymin
+        elif i ==3:
+            p = dy
+            q = ymax - y1
         
+        if p == 0: 
+            if q < 0:
+                return None
+        elif p < 0: 
+            r = q/p
+            u1 = max(u1, r)
+        else: #p > 0
+            r = q/p
+            u2 = min(u2, r)
+    
+    if u1 > u2:
+        return None
+    p1 = ( round( x1 + (u1*dx) ), round( y1+ (u1*dy) ) )
+    p2 = ( round( x1 + (u2*dx) ), round( y1+ (u2*dy) ) )
+
+    return p1, p2
+
+            
+
+
+
+
 
 def clip_shape(shape: Shape, window:Rectangle):
     post_clipping_shape_lines = []
     for i in range(-1, len(shape.vertices)-1):
-        new_points = cohen_sutherland(shape.vertices[i], shape.vertices[i+1], window.max_p, window.min_p)
+        new_points = liang_barsky(shape.vertices[i], shape.vertices[i+1], window.max_p, window.min_p)
         if new_points:
             new_line = Line()
             new_line.set_input(new_points[0])
